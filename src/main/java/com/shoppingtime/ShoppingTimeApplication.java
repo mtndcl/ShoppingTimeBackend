6e62d9e4da64e7c575171044c00c9c93b6cbab97
METIN <metindemir1215.md@gmail.com>
2023-08-09 16:22:26 +0300

    init

---
+++ src/main/java/com/shoppingtime/ShoppingTimeApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoppingtime/ShoppingTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
